74ed6ba8657cf4a41a80a2800c17548a777cdd88
Cameron Manderson <cameronmanderson@gmail.com>
2011-09-25 16:15:10 +1000

    Added a basic "more…" placeholder

---
+++ src/FlintCMS/Bundle/AdminBundle/Configuration/FragmentInterface.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Copyright Cameron Manderson (c) 2011 All rights reserved.
+ * Date: 25/09/11
+ */
+
+/**
+ *
+ * @author camm (camm@flintinteractive.com.au)
+ */
+interface FragmentInterface {
+
+}
