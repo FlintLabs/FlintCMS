bca0e75585376219496ec0c989674110aeb295fa
Cameron Manderson <cameronmanderson@gmail.com>
2011-09-25 16:15:45 +1000

    Added base configuration options for the fragment definitions

--- src/FlintCMS/Bundle/AdminBundle/Configuration/FragmentInterface.php
+++ src/FlintCMS/Bundle/AdminBundle/Configuration/FragmentInterface.php
@@ -1,13 +1,23 @@
 <?php
 /*
- * Copyright Cameron Manderson (c) 2011 All rights reserved.
- * Date: 25/09/11
+ * This file is part of the Flint CMS package
+ *
+ * (c) Cameron Manderson <camm@flintinteractive.com.au>
+ *
+ * For full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
-
+namespace FlintCMS\Bundle\AdminBundle\Configuration;
 /**
  *
  * @author camm (camm@flintinteractive.com.au)
  */
 interface FragmentInterface {
 
+    /**
+     * Returns with the alias key for the fragment
+     * @abstract
+     * @return void
+     */
+    public function getAlias();
 }
